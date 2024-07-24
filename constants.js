945834b8e4769bc8936bdc8d1bbf544b724d68aa
Axel Chaves <achaves@MBPl19C02DX8MRMD6M.localdomain>
2024-07-24 10:20:42 -0600

    feat: title app, main container of elements, root styles, and svg icon logo added

---
+++ constants.js
@@ -0,0 +1,8 @@
+const colors = {
+    "Oxford Blue": "#011638",
+    "Space Cadet": "#2E294E",
+    "Purpureus": "#9055A2", 
+    "Lilac": "#D499B9",
+    "Tea Rose": "#E8C1C5",
+    "Ultra Violet": "#4F4686"
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+
+// create HTML using template literals
+let content = `
+<h3>This is a heading</h3>
+<p>this content is meant to be added to the shadow DOM</p>
+`
+
+
+let mainContainer = document.getElementById("mainContainer")
+//let title = document.getElementById("").append(content)
+
+// select the element with a class called root
+/*
+const root = document.querySelector('.root')
+// attach a shadow DOM to the selected element
+root.attachShadow({ mode: 'open' })
+// append the html element to the shadow DOM
+root.shadowRoot.appendChild(htmlSection)*/
