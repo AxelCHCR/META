37d7777587d28a49cd3881cb54fc599402421ee1
Axel Chaves <achaves@MBPl19C02DX8MRMD6M.localdomain>
2024-07-24 16:14:51 -0600

    feat: dynamic label amounts added

--- index.js
+++ index.js
@@ -19,17 +19,39 @@ const addExpense = `
 const informationBox = `
     <div id=informationContainer>Soy una cajota</div>
 `
+const informationLabel = (label, amount) => {
+    return `
+    <h4 class=amountLabel>${label} : ${amount} </h4>
+    `
+}
 let mainContent = document.getElementById("mainContent")
-console.log(mainContent)
 const addBudgetElement = document.createElement('span') //TODO: guardar estas lineas de codigo para despues, ya que son las que resuelven el tema de agregar elementos a otros divs
 const addExpenseElement = document.createElement('span')
 const informationElement = document.createElement('span')
+
+
 addBudgetElement.innerHTML = addBudget;
 addExpenseElement.innerHTML = addExpense;
 informationElement.innerHTML = informationBox;
+
 mainContent.append(addBudgetElement)
 mainContent.append(addExpenseElement)
 mainContent.append(informationElement)
+
+const amounts =
+{
+"total amount": 2000,
+"total expenses": 1000,
+"budget left": 1000
+}
+for (const element in amounts) {
+    const textInformationElement = document.createElement('span')
+    let informationContent = document.getElementById("informationContainer")
+    textInformationElement.innerHTML = informationLabel(element, amounts[element])
+    //textInformationElement.innerHTML = informationLabel("Total Expenses", 1000)
+    informationContent.append(textInformationElement)
+}
+
 //let title = document.getElementById("").append(content)
 
 // select the element with a class called root
